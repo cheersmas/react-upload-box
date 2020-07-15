b6e824b0b12eff25d547ab258ef233dd3dbf19e3
humanbean <shradhankr0028@gmail.com>
2020-07-16 01:35:05 +0530

    :tada: inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Animate} from 'react-move';
+import {easeExpInOut} from 'd3-ease';
+
+const Line = () => {
+  const [move, setMove] = useState(true);
+  const [xOffset, setXOffset] = useState(0);
+  const updateMove = () => setMove(prev => !prev);
+  const updateXOffsetForward = () => setXOffset(prev => prev + 100);
+  const updateXOffset = () => setXOffset(prev => prev - 100);
+  return (
+    <div>
+      <button onClick={updateMove}>Move: {move.toString()}</button>
+      <button onClick={updateXOffset}>Update: {xOffset}</button>
+      <button onClick={updateXOffsetForward}>UpdateForward: {xOffset}</button>
+      <Animate
+        show={move}
+        start={{ xOffset }}
+        enter={{ xOffset: [xOffset], timing: { duration: 1000, ease: easeExpInOut } }}
+        update={{ xOffset: [xOffset], timing: { duration: 1000, ease: easeExpInOut }}}
+      >
+        {({ xOffset }) => {
+          return (
+          <svg viewBox="0 0 600 12" preserveAspectRatio="none">
+            <path
+              d={`M0,1 L${200 + xOffset},1 C${300 + xOffset},1 ${300 + xOffset},11 ${400 + xOffset},11 L600,11`}
+              stroke="currentColor"
+              fill="none"
+            />
+          </svg>)
+        }}
+      </Animate>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Line />
     </div>
   );
 }
